637c51177cace1c70a846a89db391e64d7a7f798
CarbonBeard <sharanov.pasha@gmail.com>
2017-10-11 19:08:54 +1000

    Home template

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-
+from flask import render_template
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,23 +12,15 @@ RSS_FEEDS = {'bbc': 'https://feeds.bbci.co.uk/news/rss.xml',
 
 
 @app.route("/")
-def bbc():
-	return get_news(publication = "bbc")
-
 @app.route("/<publication>")
-def get_news(publication):
+def get_news(publication="bbc"):
 	feed = feedparser.parse(RSS_FEEDS[publication])
 	if len(feed['entries']) > 0: 
 		first_article = feed['entries'][0] 
-		return """<html>
-			<body>
-			<h1>Headlines </h1>
-			<b>{0}</b> </ br>
-			<i>{1}</i> </ br>
-			<p>{2}</p> </ br>
-			</body>
-			</html>""".format(first_article.get("title"), first_article.
-			get("published"), first_article.get("summary"))
+		return render_template("home.html",
+								title=first_article.get("title"),
+								published=first_article.get("published"),
+								summary=first_article.get("summary"))
 	else:
 		return "There are no news today."
 
