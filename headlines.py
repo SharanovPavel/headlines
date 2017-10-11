4ba84e77a0b6d4b5508f5d098cb12cdf7f5871ee
CarbonBeard <sharanov.pasha@gmail.com>
2017-10-11 21:08:51 +1000

    Weather search

--- headlines.py
+++ headlines.py
@@ -1,4 +1,8 @@
 import feedparser
+import json
+import urllib2
+import urllib
+
 from flask import render_template
 from flask import Flask
 from flask import request
@@ -10,17 +14,45 @@ RSS_FEEDS = {'bbc': 'https://feeds.bbci.co.uk/news/rss.xml',
 			 'fox': 'https://feeds.foxnews.com/foxnews/latest',
 			 'iol': 'https://www.iol.co.za/cmlink/1.640'}
 
-
+DEFAULTS = {'publication': 'bbc',
+			'city': 'London, UK'}
 
 @app.route("/")
-def get_news():
-	query = request.args.get("publication")
+def home():
+	publication = request.args.get('publication')
+	if not publication:
+		publication = DEFAULTS['publication']
+	articles = get_news(publication)
+	city = request.args.get('city')
+	if not city:
+		city = DEFAULTS['city']
+	weather = get_weather(city)
+	return render_template("home.html", articles = articles, weather = weather)
+
+def get_news(query):
 	if not query or query.lower() not in RSS_FEEDS:
-		publication = "bbc"
+		publication = DEFAULTS['publication']
 	else:
 		publication = query.lower()
 	feed = feedparser.parse(RSS_FEEDS[publication])
-	return render_template("home.html", articles=feed['entries'])
+	return feed['entries']
+
+
+
+def get_weather(query):
+	api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=1bce807ab2a172f4da8a4811f814223e"
+	query = urllib.quote(query)
+	url = api_url.format(query)
+	data = urllib2.urlopen(url).read()
+	parsed = json.loads(data)
+	weather = None
+	if parsed.get("weather"):
+		weather =   {"description": parsed["weather"][0]["description"],
+					"temperature": parsed["main"]["temp"],
+					"city": parsed["name"],
+					"country": parsed['sys']['country']
+					}
+	return weather
 
 if __name__ == "__main__":
 	app.run(port=5000, debug=True)
